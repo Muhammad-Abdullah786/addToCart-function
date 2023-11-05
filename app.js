ed973a42b48910434ef205883f6d22a7b07f7f26
Muhamad Abdullah <abdulx125@gmail.com>
2023-11-05 00:57:56 -0700

    still cart not done

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let products = [
-    {imgSrc : "../assets/pic.jpg",title : "Macbook", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/iphone.jpg",title : "Iphone 99", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/sam.jpg",title : "Samsung 11.2", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/gaming.webp",title : "Gaming PC", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/lip.jpg",title : "lipistick", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/sampoo.jpg",title : "Shampoo", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/conditioner.jpg",title : "Conditioner", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/potato.svg",title : "Potato", description : "Premium Quality", price: '$89.99},
-    {imgSrc : "../assets/apple.jpg",title : "Apple", description : "Premium Quality, price: '$89.99"}
+    {imgSrc : "../assets/pic.jpg",title : "Macbook", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/iphone.jpg",title : "Iphone 99", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/sam.jpg",title : "Samsung 11.2", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/gaming.webp",title : "Gaming PC", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/lip.jpg",title : "lipistick", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/sampoo.jpg",title : "Shampoo", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/conditioner.jpg",title : "Conditioner", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/potato.svg",title : "Potato", description : "Premium Quality", price: '$89.99'},
+    {imgSrc : "../assets/apple.jpg",title : "Apple", description : "Premium Quality", price : '$89.99'}
 ];
 let mainDiv = document.getElementsByClassName('main')[0];
 products.forEach(function(product){
@@ -15,17 +15,17 @@ products.forEach(function(product){
     let img = document.createElement("img");
     let title = document.createElement("h2");
     let detail = document.createElement("p");
-// i have to make addToCart button too
-let addToCart = document.createElement("button");
-
-
+    // i have to make addToCart button too
+    let addToCart = document.createElement("button");
+    
+    
     img.src = product.imgSrc;
     img.alt = "Product Image";
     img.style.width = '90%';
     img.style.height = '60%';
     title.textContent = product.title
     detail.textContent = product.description
-
+    
     card.appendChild(img);
     card.appendChild(title);
     card.appendChild(detail);
@@ -37,15 +37,36 @@ let addToCart = document.createElement("button");
     addToCart.addEventListener('click', ()=>{// onclick will not work
         addProduct(product)
     })
-
-
+    
+    
     mainDiv.appendChild(card);
-
+    
     
 });
-let nameOfProduct = getElementsById('nameOfCart');
-function addProduct(product){ // the reason product function will not work because it's inside the foreach function 
-        console.log("added", product);
+let nameOfProduct = document.getElementById('nameOfProduct');
+// function addProduct(product){ // the reason product function will not work because it's inside the foreach function 
+//     let img = document.createElement("img");
+//     img.src = product.imgSrc;
+//     img.alt = "Product Image";
+//     img.style.width = '30px';
+//     img.style.height = '60px';
+//     img.appendChild(nameOfProduct);
+//     nameOfProduct.innerHTML += "<br>" + product.title + img.src
+
+//         console.log("added", product, nameOfProduct);
 
 
+// }
+function addProduct(product){
+    let img = document.createElement("img");
+    img.src = product.imgSrc;
+    img.alt = "Product Image";
+    img.style.width = '30px';
+    img.style.height = '60px';
+
+    let title = document.createElement("p");
+    title.textContent = product.title;
+
+    nameOfProduct.innerHTML += "<br>" + title.outerHTML + img.outerHTML;
+    console.log("Product added: ", product);
 }
